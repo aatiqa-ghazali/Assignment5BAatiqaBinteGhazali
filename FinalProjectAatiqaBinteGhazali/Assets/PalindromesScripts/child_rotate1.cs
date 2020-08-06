f5ef463fc2dae94b1362ef2e716f6a11ded5c88a
Aatiqa Bint e Ghazali <>
2020-08-05 18:44:58 -0700

    Project is done
    project is done

---
+++ FinalProjectAatiqaBinteGhazali/Assets/PalindromesScripts/child_rotate1.cs
@@ -0,0 +1,24 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class child_rotate : MonoBehaviour
+{
+    
+    public string x = "";
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(-15, -30, -45) * Time.deltaTime);
+        
+
+    }
+
+
+
+    
+}
